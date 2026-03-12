ae5a1812891b4c17f2b1388ed4560238a62de0f4
lucazamagni <lucazamagni05@gmail.com>
2026-03-12 17:45:17 +0000

    Caricamento file esercitazione 1

---
+++ esercitazione1/medie.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, const char *argv[]) //argc=numero input; argv=vettore con gli input
+{
+string filename = argv[1];
+
+ifstream file(filename); //legge il testo del file
+if ( file.is_open() ) {
+
+    string city; //assegno a city il tipo stringa
+    double temp1, temp2, temp3, temp4; //assegno alle temp il tipo double
+
+    while ( file >> city >> temp1 >> temp2 >> temp3 >> temp4 ) { //ciclo direttamente sul testo e non uso eof poichè dà problemi nel caso di infinite righe vuote
+    double media = (temp1 + temp2 + temp3 + temp4)/4;
+    cout << city << " " << media << "\n";
+    }
+}
+else {
+    cerr << "Errore nell'apertura del file\n";
+return 1;
+}
+return 0;
+}
